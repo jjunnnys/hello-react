706fcceffc0b834833b56cece8c5516a92e572ec
ppby <salswnss@gmail.com>
2020-06-29 13:56:29 +0900

    Add 'class 대신 className'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   const name = 'react';
 
-  return (
-    <div
-      style={{
-        backgroundColor: 'black', // 카멜케이스로 작성
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16, // 단위 생략하면 px로 설정
-      }}
-    >
-      {name}
-    </div>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
