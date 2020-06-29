2ed8ab2b333d8bc2441c41dc753243c25ad98acd
ppby <salswnss@gmail.com>
2020-06-29 15:06:33 +0900

    Modify 'class형 컴포넌트'

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      나의 이름은 {name}이다. <br />
-      children 값은 {children} <br />
-      좋아하는 숫자는 {favoriteNumber}
-    </div>
-  );
-};
+class MyComponent extends Component {
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        나의 이름은 {name}이다. <br />
+        children 값은 {children} <br />
+        좋아하는 숫자는 {favoriteNumber}
+      </div>
+    );
+  }
+}
 
 MyComponent.defaultProps = {
   name: "???",
