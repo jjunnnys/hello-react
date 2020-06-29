0ca050c60fcceab550051590feec49e180194b03
ppby <salswnss@gmail.com>
2020-06-29 15:49:02 +0900

    Modify 'this.setState에 객체 대신 함수 인자 전달하기'

--- src/Counter.js
+++ src/Counter.js
@@ -16,7 +16,13 @@ export default class Counter extends Component {
           // onclick을 통해 버튼이 클릭되었을 떄 호출할 함수를 지정
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있음
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            // 위 아래 기능 똑같음
+            this.setState((prevState) => ({ number: prevState.number + 1 }));
           }}
         >
           +1
