27a77ee027c78afbcd415b917bbbfde66e444301
ppby <salswnss@gmail.com>
2020-06-29 13:51:16 +0900

    Modify '인라인 스타일'

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 function App() {
   const name = 'react';
-  const style = {
-    backgroundColor: 'black', // 카멜케이스로 작성
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16, // 단위 생략하면 px로 설정
-  };
-  return <div style={style}>{name}</div>;
+
+  return (
+    <div
+      style={{
+        backgroundColor: 'black', // 카멜케이스로 작성
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16, // 단위 생략하면 px로 설정
+      }}
+    >
+      {name}
+    </div>
+  );
 }
 
 export default App;
