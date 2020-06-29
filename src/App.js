347bd6eef023c69075d41f201159f2726123a441
ppby <salswnss@gmail.com>
2020-06-29 13:36:34 +0900

    Modify && 연산자를 사용한 조건부 렌더링 (2)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const name = 'react';
-  return <>{name === 'react' ? <h1>리액트 입니다.</h1> : null}</>;
+  return <>{name === 'react' && <h1>리액트 입니다.</h1>}</>;
 }
 
 export default App;
