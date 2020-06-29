8b552c6bf96715b9acf9084850a16a841fddb60f
ppby <salswnss@gmail.com>
2020-06-29 15:57:03 +0900

    Modify 'this.setState가 끝난 후 특정 작업 실행하기'

--- src/Counter.js
+++ src/Counter.js
@@ -13,16 +13,18 @@ export default class Counter extends Component {
         <h1>{number}</h1>
         <h2>바뀌지 않는 값:{fixedNumber}</h2>
         <button
-          // onclick을 통해 버튼이 클릭되었을 떄 호출할 함수를 지정
           onClick={() => {
-            // this.setState를 사용하여 state에 새로운 값을 넣을 수 있음
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1,
-              };
-            });
-            // 위 아래 기능 똑같음
-            this.setState((prevState) => ({ number: prevState.number + 1 }));
+            this.setState(
+              (prevState) => {
+                return {
+                  number: prevState.number + 1,
+                };
+              }, // 콜백함수 지정
+              () => {
+                console.log("setState가 호출되었습니다.");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
