02b5590f0cdd5adb7ef30a3a7bc5d0ec6295058e
ppby <salswnss@gmail.com>
2020-06-29 14:48:59 +0900

    Add '비구조화 할당'

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const MyComponent = (props) => {
-  return <div>나의 이름은 {props.name}이다.</div>;
+  const { name, children } = props;
+  return (
+    <div>
+      나의 이름은 {name}이다. <br />
+      children 값은 {children}
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
