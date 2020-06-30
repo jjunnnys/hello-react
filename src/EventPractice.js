d06a795a42be7b4e1a62c8461b6749bdc5c08975
ppby <salswnss@gmail.com>
2020-06-30 11:00:41 +0900

    Add 'onChange 이벤트 핸들링하기 (class)'

---
+++ src/EventPractice.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요."
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+      </div>
+    );
+  }
+}
