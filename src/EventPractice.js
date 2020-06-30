e98f0cba754171cded04b10e5abc9b56eb1d0e4c
ppby <salswnss@gmail.com>
2020-06-30 11:08:14 +0900

    Modify 'state에 input 값 담기'

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class EventPractice extends Component {
+  state = {
+    message: '',
+  };
   render() {
     return (
       <div>
@@ -9,10 +12,15 @@ export default class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요."
+          value={this.state.message}
           onChange={(event) => {
-            console.log(event.target.value);
+            // console.log(event.target.value);
+            this.setState({
+              message: event.target.value,
+            });
           }}
         />
+        {/* <h1>{this.state.message}</h1> */}
       </div>
     );
   }
