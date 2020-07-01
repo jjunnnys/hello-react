efdcf95066896736a941359532270a99cec0f2c5
ppby <salswnss@gmail.com>
2020-07-01 11:17:12 +0900

    Modify 'map 함수 사용'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        {/* 초기 렌더링 시 scrollToBottom 값이 undefind이므로 새로운 함수를 만들어 준다. */}
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          {' '}
-          맨 밑으로
-        </button>
-      </>
-    );
+    return <IterationSample />;
   }
 }
 
